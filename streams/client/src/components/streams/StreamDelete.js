0f826fe1fefc3014a0351cbcc8a2f0a9ec83d0f1
xiaodan cui <cxd@xiaodan-MacBook-Pro.local>
2021-02-05 11:23:25 -0800

    implemented streaming video

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,7 +1,54 @@
 import React from "react";
+import Modal from "../Modal";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import history from "../../history";
 
-const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+import { fetchStream, deleteStream } from "../../actions";
+
+//this.props.fetchStream(this.props.match.params.id);
+
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderActions = () => {
+    return (
+      <React.Fragment>
+        <button
+          className="ui red button"
+          onClick={() => this.props.deleteStream(this.props.match.params.id)}
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </React.Fragment>
+    );
+  };
+
+  renderContent = () => {
+    if (!this.props.stream) {
+      return "Are your sure you want to delete this stream?";
+    }
+    return `Are your sure you want to delete this stream with title: ${this.props.stream.title}?`;
+  };
+  render() {
+    return (
+      <Modal
+        title="Delete Stream"
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={() => history.push("/")}
+      />
+    );
+  }
+}
+const mapSateToProps = (state, props) => {
+  return { stream: state.streams[props.match.params.id] };
 };
-
-export default StreamDelete;
+export default connect(mapSateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,7 +1,55 @@
 import React from "react";
+import flv from "flv.js";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+    this.builderPlayer();
+  }
+
+  componentDidUpdate() {
+    this.builderPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  builderPlayer = () => {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+    //this.player.play();
+  };
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading....</div>;
+    }
+    return (
+      <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
+        <h1>{this.props.stream.title}</h1>
+        <h5>{this.props.stream.description}</h5>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, props) => {
+  return { stream: state.streams[props.match.params.id] };
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
